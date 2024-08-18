87693ef199c3c4e21001c6b416a48563b3343f44
David Sampson <Davidchosen33@gmial.com>
2024-08-18 18:33:19 +0100

    I built an API that says Hello

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const express = require("express")
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Server started running on ${PORT}`)
+})
+
+
+
+// CRUD Authentication
+
+app.get("/", (request, response)=>{
+    response.json("Welcome to Acces bank Server")
+})
+
+
+const users = [
+    {
+        firstName: "David",
+        lastName: "Samp[sopn"
+    },
+    {
+        firstName: "David",
+        lastName: "Samp[sopn"
+    },
+    {
+        firstName: "David",
+        lastName: "Samp[sopn"
+    },
+]
+
+app.get("/users", (request, response)=>{
+
+    response.json(users)
+})
+
+app.post("/login", (request, response)=>{
+
+    const { email, password } = request.body 
+
+})
