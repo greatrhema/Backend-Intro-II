e18d7a291d9904e8c8433c8fef7632372ca14c09
David Sampson <Davidchosen33@gmial.com>
2024-08-24 21:29:30 +0100

    updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
 const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
 
 const app = express()
 
@@ -24,7 +28,7 @@ app.post('/chibuzor', (request, response)=>{
     const { email, password } = request.body
 
 
-    response.json(email)
+    response.status(200).json(email)
 })
 
 
@@ -33,10 +37,10 @@ app.put("/eze", (req, res)=>{
     const { name, state, age } = req.body
 
     if(!name){
-        return res.json({message: "Please add your a name"})
+        return res.status(400).json({message: "Please add your a name"})
     }
 
-    return res.json({message: "Successful", name})
+    return res.status(200).json({message: "Successful", name})
 
 })
 
@@ -45,23 +49,71 @@ app.post("/register", (req, res)=>{
     const { email, name, state, age, phoneNumber } = req.body
 
     if(!email){
-        return res.json({message: "Please add your email"})
+        return res.status(400).json({message: "Please add your email"})
     }
 
     if( age < 18){
-        return res.json({message: "Please you're underage"})
+        return res.status(400).json({message: "Please you're underage"})
     }
 
     if(!name){
-        return res.json({message: "Please add your name"})
+        return res.status(400).json({message: "Please add your name"})
     }
 
     const newUser = { email, name, state, age, phoneNumber}
 
     // Save to DATABASE
 
-    return res.json({message: "Registration Successful", newUser})
+    return res.status(200).json({message: "Registration Successful", newUser})
 })
+
+
+app.put("/edit_user", (req, res)=>{
+
+    const { name, email, phoneNumber } = req.body 
+
+    if(!email){
+        return res.status(400).json({message: "Please add your email."})
+    }
+
+    // Find user o Database with that email
+
+    // Update the old information with the new object
+
+    const userID = Math.floor(Math.random() * 100)
+
+    const date = new Date()
+
+    const newUser = {
+        name,
+        email,
+        userID,
+        phoneNumber,
+        date_Of_Registration: date
+    }
+
+    return res.status(200).json({
+        message: "Reg Successful", 
+        user: newUser
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+app.use((req, res)=>{
+    res.status(404).json({message: "This endpoint does not exist yet!"})
+})
+
+
+
 
 
 
